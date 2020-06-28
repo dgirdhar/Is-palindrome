25aae6b43b4429deece036a958e6a72454ff02cd
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-06-28 17:41:47 -0400

    Update main.cpp
    This solution produces better results than to_string function.
    Runtime: 12 ms, faster than 78.45% of C++ online submissions for Palindrome Number.
    Memory Usage: 5.9 MB, less than 85.19% of C++ online submissions for Palindrome Number.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,30 @@ public:
     bool isPalindrome(int x) {
         bool res = true;
         
-        string str = to_string(x);
-        int startIndex = 0;
-        int endIndex = str.size() - 1;
-        
-        while (startIndex <= endIndex) {
-            if (str[startIndex++] != str[endIndex--]) {
-                res = false;
-                break;
+        if (x < 0) {
+            res = false;
+        }
+        else {
+            int str[10];
+            int temp = x;
+            int index = 0;
+            
+            while (temp != 0) {
+                int remainder = temp % 10;
+                
+                str[index++] = remainder;
+                temp /= 10;
+            } 
+            
+            int startIndex = 0;
+            int endIndex = index - 1;
+
+            while (startIndex <= endIndex) {
+                if (str[startIndex++] != str[endIndex--]) {
+                    res = false;
+                    
+                    break;
+                }
             }
         }
         
